9241daec04e1cf0012e974c5a7e18b8e20dae4f2
Brian Shirey <brianshirey@Brians-MacBook-Pro.local>
2012-07-15 11:05:30 -0400

    Initial Commit

---
+++ BrianTetris/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BrianTetris
+//
+//  Created by Christopher Shirey on 7/15/12.
+//  Copyright (c) 2012 Self. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
